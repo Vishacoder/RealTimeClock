edd96e3b9537a0afbcd095368329196a585dd525
Rankoth Vishwa Anupama <vishwaanupama12@gmail.com>
2025-07-12 21:32:59 +0200

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace RealTimeClock
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            timer1 = new System.Windows.Forms.Timer(components);
+            lblClock = new Label();
+            SuspendLayout();
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 1000;
+            timer1.Tick += timer1_Tick;
+            // 
+            // lblClock
+            // 
+            lblClock.AutoSize = true;
+            lblClock.Dock = DockStyle.Fill;
+            lblClock.Font = new Font("Segoe UI", 24F);
+            lblClock.Location = new Point(0, 0);
+            lblClock.Name = "lblClock";
+            lblClock.Size = new Size(98, 45);
+            lblClock.TabIndex = 0;
+            lblClock.Text = "Clock";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(284, 161);
+            Controls.Add(lblClock);
+            MinimumSize = new Size(300, 200);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "The Real Time Clock";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private Label lblClock;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace RealTimeClock
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            lblClock.Text = DateTime.Now.ToString("hh:mm:ss tt");
+        }
+    }
+}
